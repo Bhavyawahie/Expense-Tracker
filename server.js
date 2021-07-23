1a1f00a28460d005d2215833046a479ab09daa3d
Bhavya Wahie <bhavyawahie@gmail.com>
2021-07-23 22:16:51 +0530

    connectDB middleware required and called inside of server.js

--- server.js
+++ server.js
@@ -3,12 +3,18 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 dotenv.config({ path: './config/config.env'});
+connectDB();
 
-const transactions = require("./routes/transactions") 
+const transactions = require("./routes/transactions"); 
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 
 app.use("/api/v1/transactions", transactions);
 
