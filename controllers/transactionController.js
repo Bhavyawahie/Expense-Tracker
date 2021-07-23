848d7829162b9895df3d77d9cbc4f6d725313f57
Bhavya Wahie <bhavyawahie@gmail.com>
2021-07-23 22:18:22 +0530

    Transaction model required inside transactionController middlware and find mthod was implemented

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -1,20 +1,45 @@
+const Transaction = require('../models/Transaction')
+
 //  @desc    Get all Transactions
 //  @Route   GET /api/v1/transactions
 //  access   Public
-exports.getTransactions = (req, res, next) => {
-    res.send("GET Transactions");
+exports.getTransactions = async (req, res, next) => {
+    try{
+        const transactions = await Transaction.find();
+        return res.status(200).json({
+            success: true,
+            count: transactions.length,
+            data: transactions
+        });
+    }
+    catch (err) {
+        return res.send(500).json({
+            success: false,
+            error: "Server Error"
+        })
+    }
 }
 
 //  @desc    Add all Transactions
 //  @Route   POST /api/v1/transactions
 //  access   Public
-exports.addTransactions = (req, res, next) => {
-    res.send("POST Transactions");
+exports.addTransactions = async (req, res, next) => {
+    try {
+        const { text, amount } =  req.body;
+        const transaction = await Transaction.create(req.body);
+        return res.send(201).json({
+            success: true,
+            data: transaction
+        });
+    }
+    catch (err) {
+        console.log(err);
+    }
 }
 
 //  @desc    Delete a Transaction
 //  @Route   DELETE /api/v1/transactions/:id
 //  access   Public
-exports.deleteTransactions = (req, res, next) => {
+exports.deleteTransactions =  (req, res, next) => {
     res.send("DELETE Transactions");
 }
