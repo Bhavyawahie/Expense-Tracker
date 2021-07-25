e50f4e7d97c7996182032010206ea3b2d38d9b7c
Bhavya Wahie <bhavyawahie@gmail.com>
2021-07-25 20:34:21 +0530

    server ready for production enviroment

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
@@ -9,14 +10,24 @@ dotenv.config({ path: './config/config.env'});
 connectDB();
 
 const transactions = require("./routes/transactions"); 
+const { dirname } = require('path');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+if(process.env.NODE_ENV ==="development") {
+    app.use(morgan('dev'))
+}
+
 
 app.use("/api/v1/transactions", transactions);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "client", "build", "index.html")));
+}
 
 const PORT = process.env.PORT || 4000
 
